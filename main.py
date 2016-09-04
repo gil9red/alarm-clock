fc3a05c433258b38edb61c7db9d8aeb18c271966
Ilya Petrash <ilya.petrash@inbox.ru>
2016-09-04 07:08:51 +0400

    Будильник починен

--- main.py
+++ main.py
@@ -4,12 +4,30 @@
 __author__ = 'ipetrash'
 
 
+# TODO: сделать версию функции для PySide/PyQt4/PyQt5
+def load_qt4_plugins():
+    """
+    Функция загружает Qt плагины.
+
+    """
+
+    import PySide
+    import os
+
+    qApp = PySide.QtGui.QApplication.instance()
+
+    for plugins_dir in [os.path.join(p, "plugins") for p in PySide.__path__]:
+        qApp.addLibraryPath(plugins_dir)
+
+
 if __name__ == '__main__':
     import sys
     from PySide.QtGui import QApplication
     from mainwindow import MainWindow
 
     app = QApplication(sys.argv)
+
+    load_qt4_plugins()
 
     mw = MainWindow('Пилигрим - Стереть.mp3')
     mw.setWindowTitle('Будильник')
