00cf33f261f6247182137a01c5e62253b36e0d48
ipetrash <ilya.petrash@inbox.ru>
2021-11-05 22:37:34 +0500

    Обновлено. Увеличение шрифта приложения на 8

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ from ui.mainwindow import MainWindow
 
 app = QApplication(sys.argv)
 
+font = app.font()
+font.setPointSize(font.pointSize() + 8)
+app.setFont(font)
+
 mw = MainWindow(r'C:\Users\ipetrash\Music\Эпоха – Ценой великих жертв.mp3')
 mw.show()
 
