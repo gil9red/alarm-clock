7054f22f036094c4766a8e0f0c29c74256fbe195
gil9red <ilya.petrash@inbox.ru>
2025-08-04 03:02:02 +0500

    Рефакторинг. config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ DIR = Path(__file__).resolve().parent
 
 SETTINGS_FILE_NAME: str = str(DIR / "settings.ini")
 
-DIR_RESOURCES: DIR = DIR / "resources"
-DIR_ICONS: DIR = DIR_RESOURCES / "icons"
+DIR_RESOURCES: Path = DIR / "resources"
+DIR_ICONS: Path = DIR_RESOURCES / "icons"
 
 MUSIC_PATH: Path = DIR_RESOURCES / "musics" / "Der Tag - mondmatt.mp3"
